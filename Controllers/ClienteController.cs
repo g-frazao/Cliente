5855c83d8ef1f0df740968e4f874095cd6e45e53
g-frazao <117534002+g-frazao@users.noreply.github.com>
2023-10-03 20:05:52 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+public class ClienteController : Controller
+{
+    private readonly DbContext _context;
+
+    public ClienteController(DbContext context)
+    {
+        _context = context;
+    }
+    public IActionResult Create()
+    {
+        return View();
+    }
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Id,Nome,Email")] ClienteController cliente)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(cliente);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(cliente);
+    }
+}
+
